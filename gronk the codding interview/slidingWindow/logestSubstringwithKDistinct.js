98a6c32d08212b0c91cbd3f0b1f03b82f4d03521
Expedito Andrade <expeditojazz@gmail.com>
2020-07-15 06:16:29 -0300

    add longestSubstringK

--- gronk the codding interview/slidingWindow/logestSubstringwithKDistinct.js
+++ gronk the codding interview/slidingWindow/logestSubstringwithKDistinct.js
@@ -1,34 +1,13 @@
 const longestSubsK = (str, k) => {
   let windowStart = 0;
-  let windowEnd = 0;
+  let hash = {};
   let count = 0;
-  let tempK = k;
-  let arr = [];
-  let result = "";
-  let sub = str[0];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== sub[sub.length - 1]) {
-      if (arr.includes(str[i])) {
-        sub += str[i];
-      } else {
-        count++;
-        k--;
-        if (k > 0) {
-          arr.push(str[i - 1]);
-          sub += str[i];
-        } else {
-          if (sub.length > result.length) result = sub;
-          k = count;
-          count = 0;
-          arr = [];
-          sub = str[i];
-        }
-      }
-    } else {
-      if (i !== 0) sub += str[i];
-    }
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (!hash[rightChar]) hash[rightChar] = 0;
+    hash[leftChar] += 1;
+    while (Object.keys(hash).length > k) {}
   }
-  return result;
 };
 
-console.log(longestSubsK("cbbebi", 2));
+console.log(longestSubsK("araaci", 2));
