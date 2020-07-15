ef620f63ff10d1139525665be57b5605a98c7fc4
Expedito Andrade <expeditojazz@gmail.com>
2020-07-15 17:31:50 -0300

    noRepeat self solution

--- gronk the codding interview/slidingWindow/noRepeat.js
+++ gronk the codding interview/slidingWindow/noRepeat.js
@@ -4,6 +4,28 @@ const noRepeat = (str) => {
   let result = 0;
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     const leftChar = str[windowEnd];
-    if (Object.keys(hash).includes())
+    if (!hash[leftChar]) hash[leftChar] = 0;
+    hash[leftChar] += 1;
+    while (check(hash) && windowStart <= windowEnd) {
+      const rightChar = str[windowStart];
+      hash[rightChar] -= 1;
+      windowStart += 1;
+    }
+    result = Math.max(result, windowEnd - windowStart + 1);
+    console.log(hash);
   }
+  return result;
 };
+
+const check = (hash) => {
+  let temp;
+  Object.keys(hash).forEach((key) => {
+    if (hash[key] > 1) {
+      temp = true;
+    }
+  });
+  if (temp !== true) temp = false;
+  return temp;
+};
+
+console.log(noRepeat("aabccbb"));
