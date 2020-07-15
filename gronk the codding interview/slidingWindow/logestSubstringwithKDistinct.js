eaf0dc2f3f5f00872b8cd21f6e24c61523d3c320
Expedito Andrade <expeditojazz@gmail.com>
2020-07-15 08:10:46 -0300

    longets substring final code

--- gronk the codding interview/slidingWindow/logestSubstringwithKDistinct.js
+++ gronk the codding interview/slidingWindow/logestSubstringwithKDistinct.js
@@ -1,13 +1,22 @@
 const longestSubsK = (str, k) => {
   let windowStart = 0;
   let hash = {};
-  let count = 0;
+  let result = 0;
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     const rightChar = str[windowEnd];
     if (!hash[rightChar]) hash[rightChar] = 0;
-    hash[leftChar] += 1;
-    while (Object.keys(hash).length > k) {}
+    hash[rightChar] += 1;
+    while (Object.keys(hash).length > k) {
+      const leftChar = str[windowStart];
+      hash[leftChar] -= 1;
+      if (hash[leftChar] === 0) delete hash[leftChar];
+      windowStart += 1;
+    }
+    result = Math.max(result, windowEnd - windowStart + 1);
   }
+  return result;
 };
 
 console.log(longestSubsK("araaci", 2));
+console.log(longestSubsK("araaci", 1));
+console.log(longestSubsK("cbbebi", 3));
