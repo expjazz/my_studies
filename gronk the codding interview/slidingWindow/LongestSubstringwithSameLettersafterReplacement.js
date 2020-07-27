cd370e080433f1eeb77f3ae91dbdd21b470ee168
Expedito Andrade <expeditojazz@gmail.com>
2020-07-27 04:56:43 -0300

    add noRepeat improved

---
+++ gronk the codding interview/slidingWindow/LongestSubstringwithSameLettersafterReplacement.js
@@ -0,0 +1,1 @@
+const longestSubsSameLetters = (str, k) => {};
--- gronk the codding interview/slidingWindow/noRepeat.js
+++ gronk the codding interview/slidingWindow/noRepeat.js
@@ -1,24 +1,39 @@
 const noRepeat = (str) => {
-  let windowStart = 0;
-  const hash = {};
-  let result = 0;
+  let windowStart = 0,
+    maxLength = 0,
+    charIndexMax = {};
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-    const leftChar = str[windowEnd];
-    if (!hash[leftChar]) hash[leftChar] = 0;
-    hash[leftChar] += 1;
-    console.log(check(hash, leftChar));
-    while (check(hash, leftChar)) {
-      const rightChar = str[windowStart];
-      hash[rightChar] -= 1;
-      windowStart += 1;
+    const rightChar = str[windowEnd];
+    if (rightChar in charIndexMax) {
+      windowStart = Math.max(windowStart, charIndexMax[rightChar]);
     }
-    result = Math.max(result, windowEnd - windowStart + 1);
+    charIndexMax[rightChar] = windowEnd;
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
   }
-  return result;
+  return maxLength;
 };
 
-const check = (hash, char) => {
-  return char in hash;
-};
+// const noRepeat = (str) => {
+//   let windowStart = 0;
+//   const hash = {};
+//   let result = 0;
+//   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//     const leftChar = str[windowEnd];
+//     if (!hash[leftChar]) hash[leftChar] = 0;
+//     hash[leftChar] += 1;
+//     console.log(check(hash, leftChar));
+//     while (check(hash, leftChar)) {
+//       const rightChar = str[windowStart];
+//       hash[rightChar] -= 1;
+//       windowStart += 1;
+//     }
+//     result = Math.max(result, windowEnd - windowStart + 1);
+//   }
+//   return result;
+// };
+
+// const check = (hash, char) => {
+//   return char in hash;
+// };
 
 console.log(noRepeat("aabccbb"));
