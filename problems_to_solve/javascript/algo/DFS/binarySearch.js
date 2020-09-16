d654ab2609d3b57858b956a57e80d9de16a1d887
Expedito Andrade <expeditojazz@gmail.com>
2020-09-16 06:27:53 -0300

    add binary search

---
+++ problems_to_solve/javascript/algo/DFS/binarySearch.js
@@ -0,0 +1,16 @@
+var search = function(nums, target) {
+  if (nums.length === 0) {
+      return -1
+  } 
+
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left <= right) {
+      let mid = Math.floor((right + left) / 2)
+      if (nums[mid] === target) return mid;
+      if (nums[mid] > target) right = mid - 1;
+      if (nums[mid] < target) left = mid + 1;
+  }
+  return -1
+};
